dca2b54d2d3e3a3ebd1b375c58b01916c65b7de6
rishabh0211 <rishabh0211@gmail.com>
2020-08-05 22:07:29 +0530

    removed static path

--- server/server.js
+++ server/server.js
@@ -12,10 +12,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const staticPath = path.join(__dirname, '../public');
+// const staticPath = path.join(__dirname, '../public');
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.static(staticPath));
-// app.use(router);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build/index.html"));
